fe4724e1a8b03e14f8bcb2f6aad531c3129802cd
Graham Cole (G) <graham.cole@infinity.co>
2020-04-09 15:44:41 +0100

    Updated shotty to use click module

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,26 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 Instances"
+    print('--- EC2 Instances ---')
+
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    print('--- End of List ---')
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    
-    for i in ec2.instances.all():
-        print(i)
+
+    list_instances()
