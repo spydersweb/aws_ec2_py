8f5e09a96ba4800da06757136a86ab76f890a973
Graham Cole (G) <graham.cole@infinity.co>
2020-04-14 10:56:40 +0100

    Added try catch around starting and stopping instances

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -87,7 +88,7 @@ def create_snapshots(project):
         print("Starting {0}...".format(i.id))
         i.start()
         i.wait_until_running()
-        
+
     print("Jobs Done!")
     return
 
@@ -124,7 +125,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0} ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -137,7 +142,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0} ".format(i.id) + str(e))
+            continue
 
     return
 
